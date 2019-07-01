5e4cdf793b0c4e074cddb68ca74ad63851ffd163
贺家麟 <jialin.he@aliensidea.com>
2019-07-01 11:35:59 +0800

    update  submodule

--- bin/build_toolkit.sh
+++ bin/build_toolkit.sh
@@ -1,3 +1,9 @@
 basepath=$(cd `dirname $0`; pwd)
 cd ../aliensboot-toolkit/cli
-go build -mod readonly -v -o $basepath/aliensboot
+
+# Enable the go modules feature
+export GO111MODULE=on
+# Set the GOPROXY environment variable
+export GOPROXY=https://goproxy.io
+
+go build -v -o $basepath/aliensboot
