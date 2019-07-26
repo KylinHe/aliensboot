126381b8760f9d6b535c51706f0f6835d1a0ee51
贺家麟 <jialin.he@aliensidea.com>
2019-07-26 10:38:21 +0800

    修复更新bug

--- bin/build_toolkit.bat
+++ bin/build_toolkit.bat
@@ -1,3 +1,8 @@
 set basepath=%~dp0
+
+set GO111MODULE=on
+set GOPROXY=https://goproxy.io
+
 cd ../aliensboot-toolkit/cli
-go build -mod readonly -v -o $basepath/aliensboot
+go build -v -o %basepath%/aliensboot.exe
+pause
